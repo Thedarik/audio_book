0419115fb6c409303b899de7739abc73a4ee0ad1
Asadbek27Code <arashi27asadbek@gmail.com>
2024-06-21 15:47:33 +0500

    Asadbek commit all color seaved

--- lib/src/core/style/colors.dart
+++ lib/src/core/style/colors.dart
@@ -18,18 +18,19 @@ sealed class AppColors {
 
   // every colors
 
-  static const allBlueColor = Color.fromRGBO(72, 56, 209, 1);
+  static const primary50 = Color.fromRGBO(72, 56, 209, 1);
   static const buttomBarTextColor = Color.fromRGBO(106, 106, 139, 1);
-  static const backrauntGrey = Color.fromRGBO(245, 245, 250, 1);
-  static const textBlackColor = Color.fromRGBO(1, 1, 4, 1);
-  static const apptextColor = Color.fromRGBO(46, 46, 93, 1);
-  static const orangeColor = Color.fromRGBO(247, 122, 85, 1);
-  static const textColor = Color.fromRGBO(46, 46, 93, 1);
-  // static const textColor = Color.fromRGBO(46, 46, 93, 1);
-  // static const textColor = Color.fromRGBO(46, 46, 93, 1);
-  // static const textColor = Color.fromRGBO(46, 46, 93, 1);
-  // static const textColor = Color.fromRGBO(46, 46, 93, 1);
-  // static const textColor = Color.fromRGBO(46, 46, 93, 1);
+  static const neutral5 = Color.fromRGBO(245, 245, 250, 1);
+  static const neutralBlack = Color.fromRGBO(1, 1, 4, 1);
+  static const accent50 = Color.fromRGBO(247, 122, 85, 1);
+  static const neutral60 = Color.fromRGBO(106, 106, 139, 1);
+  static const l0001 = Color.fromRGBO(0, 0, 0, 1);
+  static const neutral80 = Color.fromRGBO(46, 46, 93, 1);
+  static const neutral20 = Color.fromRGBO(213, 213, 227, 1);
+  static const neutralWhite = Color.fromRGBO(255, 255, 255, 1);
+  static const neutral50 = Color.fromRGBO(146, 146, 162, 1);
+  // static const neutralWhite = Color.fromRGBO(255, 255, 255, 1);
+  // static const neutralWhite = Color.fromRGBO(255, 255, 255, 1);
 
   // gredients
 }
