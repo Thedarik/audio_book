55e032431a76f524a9b806d576e28c107663f79d
Asadbek27Code <arashi27asadbek@gmail.com>
2024-06-21 15:33:49 +0500

    asadbek commit 2

--- lib/src/core/style/colors.dart
+++ lib/src/core/style/colors.dart
@@ -16,9 +16,20 @@ sealed class AppColors {
   static const deepGrey = Color.fromRGBO(115, 124, 134, 0.20);
   static const boxShadowColor = Color.fromRGBO(0, 0, 0, 0.08);
 
-
   // every colors
 
+  static const allBlueColor = Color.fromRGBO(72, 56, 209, 1);
+  static const buttomBarTextColor = Color.fromRGBO(106, 106, 139, 1);
+  static const backrauntGrey = Color.fromRGBO(245, 245, 250, 1);
+  static const textBlackColor = Color.fromRGBO(1, 1, 4, 1);
+  static const apptextColor = Color.fromRGBO(46, 46, 93, 1);
+  static const orangeColor = Color.fromRGBO(247, 122, 85, 1);
+  static const textColor = Color.fromRGBO(46, 46, 93, 1);
+  // static const textColor = Color.fromRGBO(46, 46, 93, 1);
+  // static const textColor = Color.fromRGBO(46, 46, 93, 1);
+  // static const textColor = Color.fromRGBO(46, 46, 93, 1);
+  // static const textColor = Color.fromRGBO(46, 46, 93, 1);
+  // static const textColor = Color.fromRGBO(46, 46, 93, 1);
 
   // gredients
 }
