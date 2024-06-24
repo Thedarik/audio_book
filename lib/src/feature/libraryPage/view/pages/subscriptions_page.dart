e9e163bbf74768489932ddff3f2332449fc28cf7
Asadbek27Code <arashi27asadbek@gmail.com>
2024-06-24 16:42:17 +0500

    commit by Asadbek

--- lib/src/feature/libraryPage/view/pages/subscriptions_page.dart
+++ lib/src/feature/libraryPage/view/pages/subscriptions_page.dart
@@ -4,6 +4,7 @@ import 'package:audio_book/src/core/style/text_style.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 
 class SubscriptionsPage extends StatelessWidget {
@@ -89,15 +90,24 @@ class SubscriptionsPage extends StatelessWidget {
                   height: 52.h,
                   width: double.infinity,
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Icon(
-                        Icons.check,
-                        color: AppColors.cF77A55,
+                      SizedBox(
+                        height: 32.h,
+                        width: 32.h,
+                        child: const Icon(
+                          Icons.check,
+                          color: AppColors.cF77A55,
+                        ),
                       ),
-                      Text(
-                        "Read and listen to all premium \nbooks without limits.",
-                        style: AppTextStyle.subscriptionFenceTextMedium,
+                      SizedBox(
+                        width: 16.h,
+                      ),
+                      SizedBox(
+                        height: 52.h,
+                        child: Text(
+                          "Read and listen to all premium \nbooks without limits.",
+                          style: AppTextStyle.subscriptionTakeSubtitleMedium?.copyWith(color: AppColors.c6A6A8B),
+                        ),
                       ),
                     ],
                   ),
@@ -112,15 +122,24 @@ class SubscriptionsPage extends StatelessWidget {
                   height: 52.h,
                   width: double.infinity,
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Icon(
-                        Icons.check,
-                        color: AppColors.cF77A55,
+                      SizedBox(
+                        height: 32.h,
+                        width: 32.h,
+                        child: const Icon(
+                          Icons.check,
+                          color: AppColors.cF77A55,
+                        ),
                       ),
-                      Text(
-                        "Ad-free experience on any \nplatform.",
-                        style: AppTextStyle.subscriptionTakeSubtitleMedium,
+                      SizedBox(
+                        width: 16.h,
+                      ),
+                      SizedBox(
+                        height: 52.h,
+                        child: Text(
+                          "Ad-free experience on any \nplatform.",
+                          style: AppTextStyle.subscriptionTakeSubtitleMedium?.copyWith(color: AppColors.c6A6A8B),
+                        ),
                       ),
                     ],
                   ),
@@ -130,20 +149,29 @@ class SubscriptionsPage extends StatelessWidget {
                 height: 16.h,
               ),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40),
+                padding: EdgeInsets.symmetric(horizontal: 40),
                 child: SizedBox(
                   height: 52.h,
                   width: double.infinity,
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Icon(
-                        Icons.check,
-                        color: AppColors.cF77A55,
+                      SizedBox(
+                        height: 32.h,
+                        width: 32.h,
+                        child: const Icon(
+                          Icons.check,
+                          color: AppColors.cF77A55,
+                        ),
                       ),
-                      Text(
-                        "Exclusive content from \nhand-picked creators.",
-                        style: AppTextStyle.subscriptionTakeSubtitleMedium,
+                      SizedBox(
+                        width: 16.h,
+                      ),
+                      SizedBox(
+                        height: 52.h,
+                        child: Text(
+                          "Exclusive content from \nhand-picked creators.",
+                          style: AppTextStyle.subscriptionTakeSubtitleMedium?.copyWith(color: AppColors.c6A6A8B),
+                        ),
                       ),
                     ],
                   ),
@@ -153,25 +181,115 @@ class SubscriptionsPage extends StatelessWidget {
                 height: 16.h,
               ),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40),
+                padding: EdgeInsets.symmetric(horizontal: 40),
                 child: SizedBox(
                   height: 52.h,
                   width: double.infinity,
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Icon(
-                        Icons.check,
-                        color: AppColors.cF77A55,
+                      SizedBox(
+                        height: 32.h,
+                        width: 32.h,
+                        child: const Icon(
+                          Icons.check,
+                          color: AppColors.cF77A55,
+                        ),
                       ),
-                      Text(
-                        "Cancel anytime without \nanymore charged.",
-                        style: AppTextStyle.subscriptionTakeSubtitleMedium,
+                      SizedBox(
+                        width: 16.h,
+                      ),
+                      SizedBox(
+                        height: 52.h,
+                        child: Text(
+                          "Cancel anytime without \nanymore charged.",
+                          style: AppTextStyle.subscriptionTakeSubtitleMedium?.copyWith(color: AppColors.c6A6A8B),
+                        ),
                       ),
                     ],
                   ),
                 ),
               ),
+              SizedBox(
+                height: 44.h,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                child: SizedBox(
+                  height: 126.h,
+                  width: double.infinity,
+                  child: AppImages.subscribeBannerImage,
+                ),
+              ),
+              SizedBox(
+                height: 44.h,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 80),
+                child: Text(
+                  "Access Anywhere",
+                  style: AppTextStyle.subscriptionTakeTextLarge,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 69),
+                child: Text(
+                  "One account for any platform",
+                  style: AppTextStyle.subscriptionTakeSubtitleMedium,
+                ),
+              ),
+              SizedBox(
+                height: 40.h,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: SizedBox(
+                  height: 300.h,
+                  width: double.infinity,
+                  child: SvgPicture.asset("assets/icons/svg.svg"),
+                ),
+              ),
+              SizedBox(
+                height: 48.h,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: SizedBox(
+                  child: Row(
+                    children: [
+                      Text(
+                        "Subscribe Now",
+                        style: AppTextStyle.subscriptionFenceTextMedium?.copyWith(fontSize: 24, color: AppColors.c010104),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 12.h,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: MaterialButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  color: AppColors.c4838D1,
+                  onPressed: () {},
+                  child: SizedBox(
+                    height: 56.h,
+                    width: double.infinity,
+                    child: Center(
+                      child: Text(
+                        "SUBSCRIBE",
+                        style: AppTextStyle.subscriptionBottomTextsLarge?.copyWith(color: AppColors.cFFFFFF),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 80.h,
+              )
             ],
           ),
         ],
