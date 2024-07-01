65e6e7a2fe5a56f7501b1393dc64ae3eac3073f5
Abdullah <abdulloibrohimov@gmail.com>
2024-07-01 13:56:15 +0500

    fonts done

--- lib/src/core/style/text_style.dart
+++ lib/src/core/style/text_style.dart
@@ -30,7 +30,7 @@ final class AppTextStyle extends TextTheme {
   static TextStyle? get onBoardingTitleLarge => TextStyle(
         fontWeight: FontWeight.w600,
         fontSize: FontSize.size16,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.c2E2E5D,
       );
 
@@ -38,7 +38,7 @@ final class AppTextStyle extends TextTheme {
   static TextStyle? get onBoardingSubtitleMedium => TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: FontSize.size14,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.c2E2E5D,
       );
 
@@ -46,7 +46,7 @@ final class AppTextStyle extends TextTheme {
   static TextStyle? get onBoardingButtonSkipMedium => TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: FontSize.size16,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.c4838D1,
       );
 
@@ -63,119 +63,142 @@ final class AppTextStyle extends TextTheme {
   static TextStyle? get loginTitleMedium => TextStyle(
         fontWeight: FontWeight.w600,
         fontSize: FontSize.size16,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.c2E2E5D,
       );
 
+  @override
   static TextStyle? get loginTitleMedium2 => TextStyle(
     fontWeight: FontWeight.w600,
     fontSize: FontSize.size16,
-    fontFamily: 'Rubik',
+    fontFamily: 'Poppins',
     color: AppColors.c2E2E5D,
   );
 
+  @override
   static TextStyle? get loginFieldSmall => TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: FontSize.size14,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.cB8B8C7,
       );
 
+  @override
   static TextStyle? get loginRememberSmall => TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: FontSize.size14,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.c2E2E5D,
       );
 
+  @override
   static TextStyle? get loginLoginButtonMedium => TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: FontSize.size16,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.cFFFFFF,
       );
 
+  @override
   static TextStyle? get loginForgotPasswordSmall => TextStyle(
         fontWeight: FontWeight.w600,
         fontSize: FontSize.size14,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.cF77A55,
       );
 
+  @override
   static TextStyle? get loginForgotPasswordOffSmall => TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: FontSize.size14,
-    fontFamily: 'Rubik',
+    fontFamily: 'Poppins',
     color: AppColors.c2E2E5D,
   );
+  @override
   static TextStyle? get loginOrLoginWithSmall => TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: FontSize.size14,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.c2E2E5D,
       );
 
+  @override
   static TextStyle? get loginAccountSmall => TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: FontSize.size14,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.c2E2E5D,
       );
 
+  @override
   static TextStyle? get loginAccountSmall2 => TextStyle(
     fontWeight: FontWeight.w600,
     fontSize: FontSize.size14,
-    fontFamily: 'Rubik',
+    fontFamily: 'Poppins',
     color: AppColors.cF77A55,
   );
 
 
+  @override
   static TextStyle? get loginRegisterSmall => TextStyle(
         fontWeight: FontWeight.w600,
         fontSize: FontSize.size14,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.cF77A55,
       );
 
   /// Register page
 
+  @override
   static TextStyle? get registerFieldSmall => TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: FontSize.size14,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.cF5F5FA,
       );
 
+  @override
   static TextStyle? get registerTermsSmall => TextStyle(
         fontWeight: FontWeight.w600,
         fontSize: FontSize.size14,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.c2E2E5D,
       );
+  @override
+  static TextStyle? get registerTerms2Small => TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: FontSize.size14,
+    fontFamily: 'Poppins',
+    color: AppColors.c2E2E5D,
+  );
 
+  @override
   static TextStyle? get registerTermsOrangeSmall => TextStyle(
         fontWeight: FontWeight.w600,
         fontSize: FontSize.size14,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.cF77A55  ,
       );
 
+  @override
   static TextStyle? get registerRegisterButtonMedium => TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: FontSize.size16,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.cFFFFFF,
       );
 
+  @override
   static TextStyle? get registerCancelButtonMedium => TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: FontSize.size16,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.c4838D1,
       );
 
-//
+
   /// Confirmation Page
+  @override
   static TextStyle? get registerConfirmTitleMedium => TextStyle(
         fontWeight: FontWeight.w600,
         fontSize: FontSize.size16,
@@ -183,6 +206,7 @@ final class AppTextStyle extends TextTheme {
         color: AppColors.c2E2E5D,
       );
 
+  @override
   static TextStyle? get registerConfirmSubtitleSmall => TextStyle(
         fontWeight: FontWeight.w600,
         fontSize: FontSize.size14,
@@ -190,6 +214,7 @@ final class AppTextStyle extends TextTheme {
         color: AppColors.c2E2E5D,
       );
 
+  @override
   static TextStyle? get registerConfirmFieldSmall => TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: FontSize.size14,
@@ -197,6 +222,7 @@ final class AppTextStyle extends TextTheme {
         color: AppColors.cF5F5FA,
       );
 
+  @override
   static TextStyle? get registerReceiveSmall => TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: FontSize.size14,
@@ -206,87 +232,99 @@ final class AppTextStyle extends TextTheme {
 
   /// Welcome page
 
+  @override
   static TextStyle? get welcomeTextMedium => TextStyle(
         fontWeight: FontWeight.w600,
         fontSize: FontSize.size16,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.cF77A55,
       );
 
+  @override
   static TextStyle? get welcomeBigTextLarge => TextStyle(
         fontWeight: FontWeight.w200,
         fontSize: FontSize.size48,
-        fontFamily: 'Rubik',
+        fontFamily: 'Rubik-Light.ttf',
         color: AppColors.c4838D1,
       );
 
+  @override
   static TextStyle? get welcomeSubtitleSmall => TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: FontSize.size14,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.c2E2E5D,
       );
 
   /// Personalization page
 
+  @override
   static TextStyle? get personalizationTitleMedium => TextStyle(
         fontWeight: FontWeight.w600,
         fontSize: FontSize.size16,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.c2E2E5D,
       );
 
+  @override
   static TextStyle? get personalizationSubtitleSmall => TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: FontSize.size14,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.c2E2E5D,
       );
 
+  @override
   static TextStyle? get personalizationCategorySmall => TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: FontSize.size14,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.c4838D1,
       );
 
+  @override
   static TextStyle? get personalizationCategorySmall2 => TextStyle(
     fontWeight: FontWeight.w500,
     fontSize: FontSize.size14,
-    fontFamily: 'Rubik',
+    fontFamily: 'Poppins',
     color: AppColors.c4838D1,
   );
 
+  @override
   static TextStyle? get personalizationCategorySmall3 => TextStyle(
     fontWeight: FontWeight.w500,
     fontSize: FontSize.size13,
-    fontFamily: 'Rubik',
+    fontFamily: 'Poppins',
     color: AppColors.c4838D1,
   );
 
+  @override
   static TextStyle? get personalizationEndTitleMedium => TextStyle(
         fontWeight: FontWeight.w600,
         fontSize: FontSize.size16,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.c2E2E5D,
       );
 
+  @override
   static TextStyle? get personalizationEndTwoTitleMedium => TextStyle(
     fontWeight: FontWeight.w600,
     fontSize: FontSize.size14,
-    fontFamily: 'Rubik',
+    fontFamily: 'Poppins',
     color: AppColors.c2E2E5D,
   );
 
+  @override
   static TextStyle? get personalizationEndSubtitleSmall => TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: FontSize.size14,
-        fontFamily: 'Rubik',
+        fontFamily: 'Poppins',
         color: AppColors.c2E2E5D,
       );
 
   /// Home Page
 
+  @override
   static TextStyle? get homeCategoryTextMedium => TextStyle(
         fontFamily: "Rubik",
         fontSize: FontSize.size16,
@@ -294,6 +332,7 @@ final class AppTextStyle extends TextTheme {
         color: AppColors.c010104,
       );
 
+  @override
   static TextStyle? get homeSeeMoreSmall => TextStyle(
         fontFamily: "Rubik",
         fontSize: FontSize.size14,
@@ -301,6 +340,7 @@ final class AppTextStyle extends TextTheme {
         color: AppColors.c4838D1,
       );
 
+  @override
   static TextStyle? get homeCategoriesMedium => TextStyle(
         fontFamily: "Rubik",
         fontSize: FontSize.size16,
@@ -308,6 +348,7 @@ final class AppTextStyle extends TextTheme {
         color: AppColors.c2E2E5D,
       );
 
+  @override
   static TextStyle? get homeSubtitleSmall => TextStyle(
         fontFamily: "Rubik",
         fontSize: FontSize.size12,
@@ -315,6 +356,7 @@ final class AppTextStyle extends TextTheme {
         color: AppColors.c6A6A8B,
       );
 
+  @override
   static TextStyle? get homeBookNameMedium => TextStyle(
         fontFamily: "Rubik",
         fontSize: FontSize.size16,
@@ -322,6 +364,7 @@ final class AppTextStyle extends TextTheme {
         color: AppColors.c2E2E5D,
       );
 
+  @override
   static TextStyle? get bottomBarTextSmall => TextStyle(
         fontFamily: "Rubik",
         fontSize: FontSize.size10,
@@ -331,6 +374,7 @@ final class AppTextStyle extends TextTheme {
 
   /// Search Page
 
+  @override
   static TextStyle? get searchTitleLarge => TextStyle(
         fontWeight: FontWeight.w600,
         fontSize: FontSize.size24,
@@ -338,6 +382,7 @@ final class AppTextStyle extends TextTheme {
         color: AppColors.c010104,
       );
 
+  @override
   static TextStyle? get searchRecommendationMedium => TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: FontSize.size16,
@@ -345,6 +390,7 @@ final class AppTextStyle extends TextTheme {
         color: AppColors.c010104,
       );
 
+  @override
   static TextStyle? get searchCategoryMedium => TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: FontSize.size16,
@@ -352,6 +398,7 @@ final class AppTextStyle extends TextTheme {
         color: AppColors.c2E2E5D,
       );
 
+  @override
   static TextStyle? get searchBookNameMedium => TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: FontSize.size16,
@@ -359,6 +406,7 @@ final class AppTextStyle extends TextTheme {
         color: AppColors.c2E2E5D,
       );
 
+  @override
   static TextStyle? get searchBookNameSubtitleSmall => TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: FontSize.size12,
@@ -368,18 +416,21 @@ final class AppTextStyle extends TextTheme {
 
   /// Detail Page
 
+  @override
   static TextStyle? get detailAppBarTextMedium => TextStyle(
       fontFamily: "Rubik",
       fontSize: FontSize.size16,
       fontWeight: FontWeight.w500,
       color: AppColors.c2E2E5D);
 
+  @override
   static TextStyle? get detailBookNameLarge => TextStyle(
       fontFamily: "Rubik",
       fontSize: FontSize.size20,
       fontWeight: FontWeight.w500,
       color: AppColors.c010104);
 
+  @override
   static TextStyle? get detailBookSubtitleMedium => TextStyle(
         fontFamily: "Rubik",
         fontSize: FontSize.size16,
@@ -387,24 +438,28 @@ final class AppTextStyle extends TextTheme {
         color: AppColors.c9292A2,
       );
 
+  @override
   static TextStyle? get detailCategorySmall => TextStyle(
       fontFamily: "Rubik",
       fontSize: FontSize.size12,
       fontWeight: FontWeight.w600,
       color: AppColors.c6A6A8B);
 
+  @override
   static TextStyle? get detailButtonPlaySmall => TextStyle(
       fontFamily: "Rubik",
       fontSize: FontSize.size14,
       fontWeight: FontWeight.w500,
       color: AppColors.cFFFFFF);
 
+  @override
   static TextStyle? get detailButtonReadSmall => TextStyle(
       fontFamily: "Rubik",
       fontSize: FontSize.size14,
       fontWeight: FontWeight.w500,
       color: AppColors.c4838D1);
 
+  @override
   static TextStyle? get detailSummarySmall => TextStyle(
         fontFamily: "Rubik",
         fontSize: FontSize.size14,
@@ -412,24 +467,28 @@ final class AppTextStyle extends TextTheme {
         color: AppColors.c2E2E5D,
       );
 
+  @override
   static TextStyle? get detailSummaryTextSmall => TextStyle(
       fontFamily: "Rubik",
       fontSize: FontSize.size14,
       fontWeight: FontWeight.w300,
       color: AppColors.c6A6A8B);
 
+  @override
   static TextStyle? get detailViewMoreSmall => TextStyle(
       fontFamily: "Rubik",
       fontSize: FontSize.size14,
       fontWeight: FontWeight.w500,
       color: AppColors.cF77A55);
 
+  @override
   static TextStyle? get detailAudioTitleMedium => TextStyle(
       fontFamily: "Rubik",
       fontSize: FontSize.size16,
       fontWeight: FontWeight.w500,
       color: AppColors.c010104);
 
+  @override
   static TextStyle? get detailAudioSubtitleMedium => TextStyle(
       fontFamily: "Rubik",
       fontSize: FontSize.size16,
@@ -439,6 +498,7 @@ final class AppTextStyle extends TextTheme {
 
   /// Read Mode page
 
+  @override
   static TextStyle? get readTextMedium => TextStyle(
         fontSize: FontSize.size16,
         fontWeight: FontWeight.w300,
@@ -446,6 +506,7 @@ final class AppTextStyle extends TextTheme {
         color: AppColors.c000000,
       );
 
+  @override
   static TextStyle? get readTextTitleMedium => TextStyle(
     fontSize: FontSize.size16,
     fontWeight: FontWeight.w500,
@@ -455,6 +516,7 @@ final class AppTextStyle extends TextTheme {
 
   /// Library Page
 
+  @override
   static TextStyle? get libraryTitleLarge => TextStyle(
     fontFamily: 'Rubik',
     fontWeight: FontWeight.w600,
@@ -462,6 +524,7 @@ final class AppTextStyle extends TextTheme {
     color: AppColors.c010104,
   );
 
+  @override
   static TextStyle? get libraryBookNameMedium => TextStyle(
     fontFamily: 'Rubik',
     fontWeight: FontWeight.w500,
@@ -469,6 +532,7 @@ final class AppTextStyle extends TextTheme {
     color: AppColors.c010104,
   );
 
+  @override
   static TextStyle? get libraryBookSubtitleSubtitleSmall => TextStyle(
     fontFamily: 'Rubik',
     fontWeight: FontWeight.w400,
@@ -478,6 +542,7 @@ final class AppTextStyle extends TextTheme {
 
   /// Subscriptions page
 
+  @override
   static TextStyle? get subscriptionFenceTextMedium => TextStyle(
     fontFamily: 'Rubik',
     fontSize: FontSize.size16,
@@ -485,6 +550,7 @@ final class AppTextStyle extends TextTheme {
     color: AppColors.cFFFFFF,
   );
 
+  @override
   static TextStyle? get subscriptionTitleLarge => TextStyle(
     fontFamily: 'Rubik',
     fontSize: FontSize.size32,
@@ -492,6 +558,7 @@ final class AppTextStyle extends TextTheme {
     color: AppColors.cFFFFFF,
   );
 
+  @override
   static TextStyle? get subscriptionSubscribeTextSmall => TextStyle(
     fontFamily: 'Rubik',
     fontSize: FontSize.size12,
@@ -499,6 +566,7 @@ final class AppTextStyle extends TextTheme {
     color: AppColors.cFFFFFF,
   );
 
+  @override
   static TextStyle? get subscriptionTakeTextLarge => TextStyle(
     fontFamily: 'Rubik',
     fontSize: FontSize.size24,
@@ -506,6 +574,7 @@ final class AppTextStyle extends TextTheme {
     color: AppColors.c4838D1,
   );
 
+  @override
   static TextStyle? get subscriptionTakeSubtitleMedium => TextStyle(
     fontFamily: 'Rubik',
     fontSize: FontSize.size16,
@@ -513,6 +582,7 @@ final class AppTextStyle extends TextTheme {
     color: AppColors.c010104,
   );
 
+  @override
   static TextStyle? get subscriptionBottomTextsLarge => TextStyle(
     fontFamily: 'Rubik',
     fontSize: FontSize.size16,
@@ -520,6 +590,7 @@ final class AppTextStyle extends TextTheme {
     color: AppColors.c6A6A8B,
   );
 
+  @override
   static TextStyle? get subscriptionPriceTextsLarge => TextStyle(
     fontFamily: 'Rubik',
     fontSize: FontSize.size24,
@@ -529,6 +600,7 @@ final class AppTextStyle extends TextTheme {
 
   /// Settings Page
 
+  @override
   static TextStyle? get settingProfileNameMedium => TextStyle(
     fontFamily: 'Rubik',
     fontSize: FontSize.size16,
@@ -536,6 +608,7 @@ final class AppTextStyle extends TextTheme {
     color: AppColors.c2E2E5D,
   );
 
+  @override
   static TextStyle? get settingProfileViewSmall => TextStyle(
     fontFamily: 'Rubik',
     fontSize: FontSize.size12,
@@ -543,6 +616,7 @@ final class AppTextStyle extends TextTheme {
     color: AppColors.c4838D1,
   );
 
+  @override
   static TextStyle? get settingProfileTextsSmall => TextStyle(
     fontFamily: 'Rubik',
     fontSize: FontSize.size14,
@@ -550,6 +624,7 @@ final class AppTextStyle extends TextTheme {
     color: AppColors.c2E2E5D,
   );
 
+  @override
   static TextStyle? get settingProfileLogOutMedium => TextStyle(
     fontFamily: 'Rubik',
     fontSize: FontSize.size16,
@@ -557,6 +632,7 @@ final class AppTextStyle extends TextTheme {
     color: AppColors.cF77A55,
   );
 
+  @override
   static TextStyle? get settingProfileEditTextsSmall => TextStyle(
     fontFamily: 'Rubik',
     fontSize: FontSize.size14,
@@ -564,6 +640,7 @@ final class AppTextStyle extends TextTheme {
     color: AppColors.cF77A55,
   );
 
+  @override
   static TextStyle? get settingProfileSaveMedium => TextStyle(
     fontFamily: 'Rubik',
     fontSize: FontSize.size16,
@@ -573,6 +650,7 @@ final class AppTextStyle extends TextTheme {
 
   /// Player page
 
+  @override
   static TextStyle? get playerBookNameLarge => TextStyle(
     fontSize: FontSize.size20,
     fontWeight: FontWeight.w500,
@@ -580,6 +658,7 @@ final class AppTextStyle extends TextTheme {
     color: AppColors.c010104,
   );
 
+  @override
   static TextStyle? get playerBookSubtitleMedium => TextStyle(
     fontSize: FontSize.size16,
     fontWeight: FontWeight.w400,
@@ -587,6 +666,7 @@ final class AppTextStyle extends TextTheme {
     color: AppColors.c010104,
   );
 
+  @override
   static TextStyle? get playerTimeSmall => TextStyle(
     fontSize: FontSize.size10,
     fontWeight: FontWeight.w400,
@@ -594,6 +674,7 @@ final class AppTextStyle extends TextTheme {
     color: AppColors.cBBB1FA,
   );
 
+  @override
   static TextStyle? get playerBottomTextsSmall => TextStyle(
     fontSize: FontSize.size10,
     fontWeight: FontWeight.w400,
